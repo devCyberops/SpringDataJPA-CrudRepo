93ac625bf093c085c8a3441cb32c921823fa293e
Arije BHS <113905860+devCyberops@users.noreply.github.com>
2022-11-13 09:37:14 +0100

    Add files via upload

---
+++ src/test/java/tn/esprit/rh/achat/services/CartegorieServiceImplTest.java
@@ -0,0 +1,67 @@
+package tn.esprit.rh.achat.services;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+import lombok.extern.slf4j.Slf4j;
+import tn.esprit.rh.achat.entities.CategorieProduit;
+import tn.esprit.rh.achat.services.ICategorieProduitService;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+@SpringBootTest
+@Slf4j
+public class CartegorieServiceImplTest {
+	@Autowired
+	ICategorieProduitService categorieProduitService;
+	
+	@Test
+	public void testAddCategorie() throws ParseException {
+		CategorieProduit cat = new CategorieProduit();
+		cat.setCodeCategorie("CAT2");
+		cat.setLibelleCategorie("categorie 2");
+		categorieProduitService.addCategorieProduit(cat);
+		log.info("categorie ajouter avec success");
+	}
+	
+	@Test
+	public void testModifierCategorie() throws ParseException {
+		CategorieProduit cat = new CategorieProduit();
+		cat.setCodeCategorie("CAT2");
+		cat.setLibelleCategorie("categorie 2");
+		categorieProduitService.addCategorieProduit(cat);
+		log.info("categorie ajouter avec success");
+		cat.setCodeCategorie("CAT5");
+		cat.setLibelleCategorie("categorie 5");
+		categorieProduitService.updateCategorieProduit(cat);
+		log.info("categorie modifier avec success");
+	}
+	
+	@Test
+	public void testDeleteCategorie() throws ParseException {
+		CategorieProduit cat = new CategorieProduit();
+		cat.setCodeCategorie("CAT2");
+		cat.setLibelleCategorie("categorie");
+		categorieProduitService.addCategorieProduit(cat);
+		categorieProduitService.deleteCategorieProduit(cat.getIdCategorieProduit());
+		log.info("categorie supprimer avec success");
+	}
+	
+	@Test
+	public void testRetrieveAllCategorie() throws ParseException {
+		List<CategorieProduit> listCategorie = categorieProduitService.retrieveAllCategorieProduits();
+		log.info("Nombre categorie: " + listCategorie.size()+" \n");
+		for(int i=0;i<listCategorie.size();i++){
+			log.info(""+listCategorie.get(i).getLibelleCategorie());
+		}
+	}
+	
+}
